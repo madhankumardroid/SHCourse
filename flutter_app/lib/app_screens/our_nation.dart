325970243ec52d4fdf5dbffbfac640d88766dd90
Madhan <madhan@mallow-tech.com>
2019-03-31 22:31:06 +0530

    Learned 1. Rows and Columns. 2. Expanded widgets.

---
+++ flutter_app/lib/app_screens/our_nation.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class Nation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.lightGreen,
+        padding: EdgeInsets.only(left: 10.0, top: 40.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                  "Our Nation",
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 30.0,
+                      fontFamily: "Raleway",
+                      fontWeight: FontWeight.w700,
+                      color: Colors.indigo),
+                ))
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                //In order to make the contents fit within the available space in the main axi, we need to wrap them inside the Expanded widget.
+                Expanded(
+                  child: Text("Tiger",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black)),
+                ),
+                Expanded(
+                  child: Text(
+                    "The national animal",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 20,
+                        fontFamily: "Raleway",
+                        fontStyle: FontStyle.italic),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                //In order to make the contents fit within the available space in the main axi, we need to wrap them inside the Expanded widget.
+                Expanded(
+                  child: Text("Peacock",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 20,
+                          fontFamily: "Raleway",
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black)),
+                ),
+                Expanded(
+                  child: Text(
+                    "The national bird",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 20,
+                        fontFamily: "Raleway",
+                        fontStyle: FontStyle.italic),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'app_screens/home.dart';
+import 'app_screens/our_nation.dart';
 
 void main() {
   runApp(new MaterialApp(
     title: "Exploring UI Widgets",
-    home: new Home(),
+    home: new Nation(),
   ));
 }
