82d9935be955030c7d9fe2612ebe2d31d3471505
Madhan <madhan@mallow-tech.com>
2019-03-31 18:54:28 +0530

    For learning Box Widgets(like Center widget), applying margin and padding.

---
+++ flutter_app/lib/app_screens/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            alignment: Alignment.center,
+            color: Colors.deepOrange,
+//The width and height are completely ignored.
+            //Each widget in Flutter has some dimension. We can call a widget as a box. So, this container is a box.
+            //Each box in widget follows some guidelines to attain some width and height depending on the child and parent's width and height.
+            //Such guidelines are called Box Constraints.
+            //Here the Container widget is the child of the MaterialApp widget. The width and height of the MaterialApp is being passed to the Container widget.
+            //The container widget instead of using its own width and height, acquiring the height and width of its parent widget(MaterialApp).
+            //To make the container use its own width and height instead of its parent, we need to use center widget. The center widget gives priority to the width and height of its child widget.
+//        width: 200.0,
+//        height: 200.0,
+            //Commenting for apply margin
+//            width: 200.0,
+//            height: 100.0,
+            margin: EdgeInsets.all(20.0),
+            child: Text("The container", textDirection: TextDirection.ltr)));
+  }
+}
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/app_screens/first_screen.dart';
 
-void main() => runApp(MyApp());
+import 'app_screens/home.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    //This method is called by the framework when MyApp() is called
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        //To remove debug mode banner while running the app
-        title: "My Flutter App",
-        home: Scaffold(
-            appBar: AppBar(title: Text("First Screen")), body: FirstScreen()));
-  }
+void main() {
+  runApp(new MaterialApp(
+    title: "Exploring UI Widgets",
+    home: new Home(),
+  ));
 }
