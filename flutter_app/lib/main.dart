34287068801ffeaaf7171d61a863189c5cec81f1
Madhan <madhan@mallow-tech.com>
2019-04-07 19:48:45 +0530

    Learned long lists(especially the itemBuilder).

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -5,47 +5,33 @@ void main() {
       title: "Exploring ListView",
 //    home: new Nation(), //Commented this in order to learn list view.
       home: Scaffold(
-          appBar: AppBar(title: Text("Basic list view")),
+          appBar: AppBar(title: Text("Long List")),
           body:
           getListView() // Directly calling getListView() will throw an error because we can use scrollable widgets only with in the Scaffold widget. Because the scrollble widgets might overflow beyond the screen. So, should not use the scrollable widget directly for the home property
       )));
 }
 
+List<String> getListElements() {
+  var items = List<String>.generate(
+      1000, (counter) => "Item $counter"); //To generate a list of 1000 items.
+  return items; //items is the datasource for our listview.(This is the step 1)
+}
+
 Widget getListView() {
-  var listView = ListView(
-    //The basic listview is meant for few list items. It is not suitable for handling large number of list items.
-    //It loads all item in memory when attached to screen. Since it is not memory efficient for large lists.
-    children: <Widget>[
-      //The most commonly used widget in the list view is the ListTile
-      ListTile(
-          leading: Icon(Icons.landscape),
-          title: Text("Landscape"),
-          subtitle: Text("Beautiful view"),
-          trailing: Icon(Icons.wb_sunny),
-          //The leading and trailing properties expects a widget basically the Icon widget, but we can use any widget we wish.
+  var listItems = getListElements();
+  var listView = ListView.builder(
+    //itemBuilder plays a significant role in creating our list items
+      itemBuilder: (context, index) {
+        //A function - it will be called by the ListView.builder() only for the elements that are visible on the screen. Thus it is memory efficient. It will be called only once for an element.
+        //Define what would be our list item
+        return ListTile(
+          leading: Icon(Icons.arrow_right),
+          title: Text(listItems[index]),
+          //For all the thousand elements, this statement will be executed
           onTap: () {
-            //To handle the click event of the list item
-            debugPrint("Landscape tapped");
-          }),
-      ListTile(
-        leading: Icon(Icons.access_alarm),
-        title: Text("Alarm"),
-        subtitle: Text("Beautiful view"),
-        onTap: () {
-          debugPrint("Alarm tapped");
-        },
-        //The properties are optional
-      ),
-      ListTile(
-          leading: Icon(Icons.phone),
-          title: Text("Phone"),
-          subtitle: Text("Beautiful view"),
-          trailing: Icon(Icons.wb_sunny)
-        //The leading and trailing properties expects a widget basically the Icon widget, but we can use any widget we wish.
-      ),
-      //It is not mandatory to use ListTile as the children of list view. We can use any widget of our choice.
-      Text("I'm an element")
-    ],
-  );
+            debugPrint('${listItems[index]} was tapped');
+          },
+        );
+      }); //builder is needed to create a long list.
   return listView;
 }
